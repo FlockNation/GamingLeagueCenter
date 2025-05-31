bf592f176091f1dc6ce5b96bb1bc6e4e42b2ecea
FlockNation <seshrohan@gmail.com>
2025-05-30 22:32:05 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -13,56 +13,83 @@ def run_simulation(league):
         for conf in [canada_conf, usa_conf]:
             for i in range(len(conf)):
                 for j in range(i + 1, len(conf)):
-                    for _ in range(3):  # each pair plays 3 games
+                    for _ in range(3):
                         matchups.append((conf[i], conf[j]))
 
-    else:  # Default or 'IGL'
-        teams = ['Colorado', 'Philadelphia', 'Alaska', 'Georgia', 'Miami']
-        matchups = [(teams[i], teams[j]) for i in range(len(teams)) for j in range(i + 1, len(teams))]
+        wins = defaultdict(int)
+        for t1, t2 in matchups:
+            winner = random.choice([t1, t2])
+            wins[winner] += 1
 
-    wins = defaultdict(int)
+        canada_standings = [(team, wins.get(team, 0)) for team in canada_conf]
+        usa_standings = [(team, wins.get(team, 0)) for team in usa_conf]
 
-    for t1, t2 in matchups:
-        winner = random.choice([t1, t2])
-        wins[winner] += 1
+        canada_standings.sort(key=lambda x: x[1], reverse=True)
+        usa_standings.sort(key=lambda x: x[1], reverse=True)
 
-    standings = [(team, wins.get(team, 0)) for team in teams]
-    standings.sort(key=lambda x: x[1], reverse=True)
+        overall_standings = [(team, wins.get(team, 0)) for team in teams]
+        overall_standings.sort(key=lambda x: x[1], reverse=True)
 
-    if league == 'SLOG':
-        playoff_teams = [team for team, _ in standings[:5]]
+        playoff_teams = [team for team, _ in overall_standings[:5]]
         seed1, seed2, seed3, seed4, seed5 = playoff_teams
 
         q1_winner = random.choice([seed2, seed3])
         q1_loser = seed3 if q1_winner == seed2 else seed2
 
-        elim1_winner = random.choice([seed4, seed5])
-        elim2_winner = random.choice([q1_loser, elim1_winner])
+        elim_winner = random.choice([seed4, seed5])
+        q2_winner = random.choice([q1_loser, elim_winner])
 
-        final_teams = (seed1, q1_winner) if random.choice([True, False]) else (seed1, elim2_winner)
+        final_teams = (seed1, q1_winner, q2_winner)
         champion = random.choice(final_teams)
 
         semis = {
             'Qualifier 1': (seed2, seed3),
-            'Eliminator 1': (seed4, seed5),
-            'Eliminator 2': (q1_loser, elim1_winner)
+            'Eliminator': (seed4, seed5),
+            'Qualifier 2': (q1_loser, elim_winner)
+        }
+
+        lottery = [team for team, _ in reversed(overall_standings) if team not in final_teams]
+
+        return {
+            'matchups': matchups,
+            'standings': {
+                'Canada': canada_standings,
+                'USA': usa_standings
+            },
+            'playoffs': {
+                'semis': semis,
+                'final': final_teams,
+                'champion': champion
+            },
+            'lottery': lottery
         }
 
     else:
+        teams = ['Colorado', 'Philadelphia', 'Alaska', 'Georgia', 'Miami']
+        matchups = [(teams[i], teams[j]) for i in range(len(teams)) for j in range(i + 1, len(teams))]
+
+        wins = defaultdict(int)
+        for t1, t2 in matchups:
+            winner = random.choice([t1, t2])
+            wins[winner] += 1
+
+        standings = [(team, wins.get(team, 0)) for team in teams]
+        standings.sort(key=lambda x: x[1], reverse=True)
+
         team1, team2 = standings[0][0], standings[1][0]
         final_teams = (team1, team2)
         champion = random.choice(final_teams)
         semis = {}
 
-    lottery = [team for team, _ in reversed(standings) if team not in final_teams]
+        lottery = [team for team, _ in reversed(standings) if team not in final_teams]
 
-    return {
-        'matchups': matchups,
-        'standings': standings,
-        'playoffs': {
-            'semis': semis,
-            'final': final_teams,
-            'champion': champion
-        },
-        'lottery': lottery
-    }
+        return {
+            'matchups': matchups,
+            'standings': standings,
+            'playoffs': {
+                'semis': semis,
+                'final': final_teams,
+                'champion': champion
+            },
+            'lottery': lottery
+        }
